48600c9e0ac983cea7e3326f729117ef73e21ec9
William Fulton <williamfulton@Williams-MacBook-Air.local>
2024-11-06 16:14:45 -0800

    [chore]: remove postgre db

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,7 +2,6 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { ProductsTable } from './products-table';
-import { getProducts } from '@/lib/db';
 
 export default async function ProductsPage(
   props: {
@@ -12,10 +11,9 @@ export default async function ProductsPage(
   const searchParams = await props.searchParams;
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { products, newOffset, totalProducts } = await getProducts(
-    search,
-    Number(offset)
-  );
+  const products: any[] = [];
+  const newOffset = 0;
+  const totalProducts = 0;
 
   return (
     <Tabs defaultValue="all">
