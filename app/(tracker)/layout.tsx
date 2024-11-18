a38f8a171dd4f8f32a10f925173a44c17ca8bf1d
William Fulton <wbfulton@me.com>
2024-11-18 10:53:26 -0800

    [chore]: how gov works wip

--- app/(tracker)/layout.tsx
+++ app/(tracker)/layout.tsx
@@ -1,10 +1,10 @@
 import { Analytics } from "@vercel/analytics/react";
 import { DesktopSideNavbar } from "app/(tracker)/layout/DesktopSideNavbar";
+import { MobileSideNavbar } from "app/(tracker)/layout/MobileSideNavbar";
+import { DashboardBreadcrumb } from "./layout/DashboardBreadcrumb";
 import Providers from "./providers";
 import { SearchInput } from "./search";
 import { User } from "./user";
-import { MobileSideNavbar } from "app/(tracker)/layout/MobileSideNavbar";
-import { DashboardBreadcrumb } from "./layout/DashboardBreadcrumb";
 
 export default function DashboardLayout({
   children,
@@ -15,14 +15,14 @@ export default function DashboardLayout({
     <Providers>
       <main className="flex min-h-screen w-full flex-col bg-muted/40">
         <DesktopSideNavbar />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="flex flex-col sm:gap-4 sm:pt-4 sm:pl-14">
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileSideNavbar />
             <DashboardBreadcrumb />
             <SearchInput />
             <User />
           </header>
-          <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
+          <main className="grid flex-1 items-start gap-2 sm:py-0 md:gap-4 bg-muted/40">
             {children}
           </main>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ export default {
       },
     },
     extend: {
+      backgroundImage: {
+        capitol:
+          "url('https://images.unsplash.com/photo-1720986314795-88b7260a779b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
